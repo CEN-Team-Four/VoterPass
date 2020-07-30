b614300c6a9e041650c2806118cd0b327019938b
Christian Ortega <christianmortega7@gmail.com>
2020-07-29 22:30:25 -0400

    Update CSS for home page

--- voter-pass/src/components/TimeTable/index.js
+++ voter-pass/src/components/TimeTable/index.js
@@ -3,27 +3,32 @@ import Table from 'react-bootstrap/Table';
 
 import './index.css';
 
-function TimeTable(props){   
+function TimeTable(props) {
     let timeslots = props.timeslots;
     let availability = props.availability;
-    let listItems1 = timeslots.map(item => {
-        return <tr><td>{item}</td></tr>
-    });
-    let listItems2 = availability.map(item => {        
-        return <tr><td>{item}</td></tr>;
+    let listItems = [];
+    for(let i = 0; i < timeslots.length; i++) {
+        listItems.push([timeslots[i], availability[i]]);
+    }
+    let tableRows = listItems.map(item => {
+        return (
+            <tr>
+                <td>{item[0]}</td>
+                <td>{item[1]}</td>
+            </tr>
+        );
     });
 
-    return(
-        <Table striped className="time-table table-striped table-bordered ">
-            <thead>
-                <th className="Timeslots">Timeslots</th>
-                <th className="Availability">Availability</th>
+    return (
+        <Table striped className="time-table table-striped table-bordered" style={{marginBottom: 0}}>
+            <thead className="table-head">
+                <tr>
+                    <th className="Timeslots">Timeslots</th>
+                    <th className="Availability">Availability</th>
+                </tr>
             </thead>
-            <tbody>
-                              
-                <td>{listItems1}</td>
-                <td>{listItems2}</td>
-               
+            <tbody className="table-body">
+                {tableRows}
             </tbody>
         </Table>
     );
