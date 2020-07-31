23b350f6ce73766f4f7df6e8128088a2f37da382
kbradle <50191096+kbradle@users.noreply.github.com>
2020-07-31 15:00:26 -0400

    cleaned up newQR index.js

--- voter-pass/src/components/NewQR/index.js
+++ voter-pass/src/components/NewQR/index.js
@@ -9,17 +9,7 @@ var QRCode = require('qrcode.react');
 class NewQR extends Component {
     state={
         curTime : new Date(),
-        meridiem: this.intitializeMerideiem(),
         timSlot: this.props.location.state.timSlot
-    }
-
-    intitializeMerideiem(){
-      let time = new Date()
-      if(time.getHours() >= 12){
-        return ' P.M.'
-      }
-      else
-        return ' A.M.'
     }
 
   render() {
@@ -37,9 +27,6 @@ class NewQR extends Component {
                   <h3>Date: {this.state.curTime.getMonth() + 1}/
                    {this.state.curTime.getDate()}/
                    {this.state.curTime.getFullYear()} </h3>
-                  <h3>Current Time: {this.state.curTime.getHours()}:
-                  {this.state.curTime.getMinutes()} 
-                  {this.state.meridiem}</h3>
                   <h3>Assigned Time: {this.state.timSlot}</h3>
                 <Button className="btn" variant="dark" href="/printpage">Print</Button>
                 <Button className="btn" variant="dark" href="/">Return to table</Button>
