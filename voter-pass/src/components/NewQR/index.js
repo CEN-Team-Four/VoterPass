85e9c6b9f7f9bb28001d3a508396532831c2ca04
Christian Ortega <christianmortega7@gmail.com>
2020-07-31 13:19:38 -0400

    NewQR: Delete Current Time for generated QR Code

--- voter-pass/src/components/NewQR/index.js
+++ voter-pass/src/components/NewQR/index.js
@@ -37,9 +37,6 @@ class NewQR extends Component {
                   <h3>Date: {this.state.curTime.getMonth() + 1}/
                    {this.state.curTime.getDate()}/
                    {this.state.curTime.getFullYear()} </h3>
-                  <h3>Current Time: {this.state.curTime.getHours()}:
-                  {this.state.curTime.getMinutes()} 
-                  {this.state.meridiem}</h3>
                   <h3>Assigned Time: {this.state.timSlot}</h3>
                 <Button className="btn" variant="dark" href="/printpage">Print</Button>
                 <Button className="btn" variant="dark" href="/">Return to table</Button>
